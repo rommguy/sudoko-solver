6717a5d7fffd62194be4d6ff5f10bee030ec424f
rommguy <rommguy@gmail.com>
2023-09-16 21:50:45 +0300

    hard level

--- src/App.tsx
+++ src/App.tsx
@@ -51,8 +51,61 @@ const createInitialBoard2 = () => {
   return board;
 };
 
+const createInitialBoard3 = () => {
+  const board: BoardWithOptions = [];
+  for (let rowIndex = 0; rowIndex < 9; rowIndex++) {
+    const row: RowWithOptions = [];
+    for (let colIndex = 0; colIndex < 9; colIndex++) {
+      row.push({
+        value: null,
+        type: "initial",
+        rowIndex,
+        colIndex,
+        options: Array(10).fill(true),
+      });
+    }
+    board.push(row);
+  }
+
+  board[0][6].value = 3;
+  board[0][7].value = 6;
+  board[0][8].value = 8;
+
+  board[1][2].value = 3;
+  board[1][3].value = 5;
+  board[1][4].value = 4;
+  board[1][6].value = 7;
+  board[1][8].value = 2;
+
+  board[2][4].value = 9;
+  board[2][8].value = 1;
+
+  board[3][1].value = 8;
+  board[3][2].value = 6;
+  board[3][5].value = 7;
+
+  board[5][3].value = 3;
+  board[5][6].value = 1;
+  board[5][7].value = 4;
+
+  board[6][0].value = 9;
+  board[6][4].value = 3;
+
+  board[7][0].value = 7;
+  board[7][2].value = 2;
+  board[7][4].value = 1;
+  board[7][5].value = 5;
+  board[7][6].value = 6;
+
+  board[8][0].value = 8;
+  board[8][1].value = 3;
+  board[8][2].value = 1;
+
+  return board;
+};
+
 export const App = () => {
-  const [board, setBoard] = useState<BoardWithOptions>(createInitialBoard2());
+  const [board, setBoard] = useState<BoardWithOptions>(createInitialBoard3());
 
   const onSolveClicked = () => {
     const solvedBoard = solveBoard(board);
