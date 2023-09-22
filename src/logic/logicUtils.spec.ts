1af227f3ec414aa08f385c963dec78a773d79a62
rommguy <rommguy@gmail.com>
2023-09-22 08:16:23 +0300

    first test

--- src/logic/logicUtils.spec.ts
+++ src/logic/logicUtils.spec.ts
@@ -1,1 +1,25 @@
-describe("logicUtils", () => {});
+import { parseBoardInput } from "../utils/parseUtils.ts";
+import { solveBoard } from "./solution.ts";
+
+describe("logicUtils", () => {
+  describe("single option for value", () => {
+    it("should add value", () => {
+      const board = parseBoardInput(
+        `
+      1 2 3 5 - 9
+      4 - 6 1 2 3
+      8 9 7 - - -
+      `,
+        9,
+      );
+
+      const result = solveBoard(board);
+
+      expect(result[0][4].value).toBe(7);
+
+      const result2 = solveBoard(result);
+
+      expect(result2[1][1].value).toBe(5);
+    });
+  });
+});
